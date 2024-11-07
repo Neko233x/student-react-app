6e19a82a2168cca9419b3b727ce829caf80fdf47
Jing Wang <jingwangw12@gmail.com>
2024-11-06 20:15:04 -0500

    change form

--- src/pages/StudentSumbitPage.jsx
+++ src/pages/StudentSumbitPage.jsx
@@ -1,61 +1,57 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const StudentSumbitPage = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [major, setMajor] = useState("");
-  const [school, setSchool] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data) => {
+    await fetch(`${import.meta.env.VITE_API_URL}/students`, {
+      body: JSON.stringify({
+        firstName: data.firstName,
+        lastName: data.lastName,
+        major: data.major,
+        school: data.school,
+      }),
+      headers: {
+        "content-type": "application/json",
+      },
+      method: "POST",
+    });
+  };
 
   // 1. add onChange handlers to each input to update the state
   // 2. add onSubmit handler to the form to submit the data
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <input
         type="text"
         placeholder="First Name"
-        onChange={(e) => setFirstName(e.target.value)}
+        {...register("firstName")}
         value={firstName}
       />
       <input
         type="text"
         placeholder="Last Name"
-        onChange={(e) => setLastName(e.target.value)}
+        {...register("lastName")}
         value={lastName}
       />
       <input
         type="text"
         placeholder="Major"
-        onChange={(e) => setMajor(e.target.value)}
+        {...register("major")}
         value={major}
       />
       <input
         type="text"
         placeholder="School"
-        onChange={(e) => setSchool(e.target.value)}
+        {...register("school")}
         value={school}
       />
-      <button
-        type="button"
-        onClick={async (e) => {
-          console.log({ firstName, lastName });
-
-          await fetch(`${import.meta.env.VITE_API_URL}/students`, {
-            body: JSON.stringify({
-              firstName: firstName,
-              lastName: lastName,
-              major: major,
-              school: school,
-            }),
-            headers: {
-              "content-type": "application/json",
-            },
-            method: "POST",
-          });
-        }}
-      >
-        Submit
-      </button>
+      <button type="button">Submit</button>
     </form>
   );
 };
